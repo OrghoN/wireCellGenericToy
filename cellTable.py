8f95ed5877bfaeaca91f0cf8bee2a9cc5349a0bb
OrghoN <anoronyo@gmail.com>
2018-07-24 17:08:34 -0400

    getting merged wires from list of wire numbers

--- cellTable.py
+++ cellTable.py
@@ -12,7 +12,7 @@ def rotate(point, angle):
     return Point(point.x*math.cos(angle)+point.y*(math.sin(angle)),point.x*(-math.sin(angle))+point.y*math.cos(angle))
 
 def wireNumberFromPoint(plane, point):
-    return math.floor(math.cos(plane.angle)*point.x+math.sin(plane.angle)*point.y-math.cos(plane.angle)*palne.translationFactor)
+    return math.floor(math.cos(plane.angle)*point.x+math.sin(plane.angle)*point.y-math.cos(plane.angle)*plane.translationFactor)
 
 def generatePlaneInfo(wirePitches, volume):
     individualAngle = math.pi/len(wirePitches)
@@ -28,15 +28,43 @@ def generatePlaneInfo(wirePitches, volume):
             noOfWires = math.floor((math.sin(angle)*volume.height - math.cos(angle)*translationFactor)/pitch)
         else:
             translationFactor = 0
-            noOfWires = math.floor((math.cos(angle)*volume.width + math.sin(angle)*volume.height - math.cos(angle)*translationFactor)/pitch)
+            noOfWires = math.floor((math.cos(angle)*volume.width + math.sin(angle)*volume.height)/pitch)
 
         planes.append(PlaneInfo(angle,pitch,noOfWires,translationFactor))
 
     return planes
 
+def generateCells(planes, volume):
+    #find out min pitch
+    minPitch = min(planes, key = lambda t: t.pitch).pitch
+
+    yNumber = math.ceil(volume.height/minPitch)
+    xNumber = math.ceil(volume.width/minPitch)
+
+    i = 0
+
+    for y in range(0,yNumber):
+        for x in range(0,xNumber):
+            point = Point(x*minPitch+minPitch/2,y*minPitch+minPitch/2)
+            if(i%1000==0):
+                print(i)
+            i+=1
+            for plane in planes:
+                wireNumberFromPoint(plane, point)
+
+    return False
+
+def createMergedWires(firedWirePrimitives):
+    firedWirePrimitives = sorted(set(firedWirePrimitives))
+    gaps = [[s, e] for s, e in zip(firedWirePrimitives, firedWirePrimitives[1:]) if s+1 < e]
+    edges = iter(firedWirePrimitives[:1] + sum(gaps, []) + firedWirePrimitives[-1:])
+    return list(zip(edges, edges))
+
 volume = DetectorVolume(1000.0, 1000.0)
 wirePitches = [5.0,5.0,5.0,5.0]
 
-planes = generatePlaneInfo(wirePitches,volume)
+pprint(createMergedWires([5,6,7,8,11,14,15,16,22,23,51,53,54]))
 
-pprint(planes)
+# planes = generatePlaneInfo(wirePitches,volume)
+# generateCells(planes,volume)
+# pprint(planes)
