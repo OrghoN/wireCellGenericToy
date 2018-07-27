6bf0f046cb7058e93fcc16ab8acbc1b8b08181be
OrghoN <anoronyo@gmail.com>
2018-07-27 16:36:33 -0400

    cached angle calculations and generated 2 points on line based on merged wire

--- cellTable.py
+++ cellTable.py
@@ -6,7 +6,7 @@ import math
 
 # define data Structures
 Point = namedtuple('Point', ['x', 'y'])
-PlaneInfo = namedtuple('PlaneInfo', ['angle', 'pitch', 'noOfWires', 'translationFactor'])
+PlaneInfo = namedtuple('PlaneInfo', ['angle', 'pitch', 'noOfWires', 'translationFactor', 'sin', 'cos', 'gradient'])
 DetectorVolume = namedtuple('DetectorVolume', ['width', 'height'])
 
 def generatePlaneInfo(wirePitches, volume):
@@ -16,21 +16,35 @@ def generatePlaneInfo(wirePitches, volume):
     translationPoint = Point(volume.width, 0)
 
     for planeNo, pitch in enumerate(wirePitches):
+        #basic angle calculations
         angle = individualAngle * (planeNo + 1)
+        cos = math.cos(angle)
+        sin = math.sin(angle)
 
+        #Origin and number of wire calculations
         if (rotate(translationPoint, angle).x < 0):
             translationFactor = volume.width
             noOfWires = math.floor(
-                (math.sin(angle) * volume.height - math.cos(angle) * translationFactor) / pitch)
+                (sin * volume.height - cos * translationFactor) / pitch)
         else:
             translationFactor = 0
             noOfWires = math.floor(
-                (math.cos(angle) * volume.width + math.sin(angle) * volume.height) / pitch)
+                (cos * volume.width + sin * volume.height) / pitch)
 
-        planes.append(PlaneInfo(angle, pitch, noOfWires, translationFactor))
+        #gradient calculations
+        if(math.isclose(angle, math.pi, rel_tol=1e-5)):
+            gradient = "INF"
+        elif translationFactor == 0:
+            gradient = math.tan(angle+math.pi/2)
+        else:
+            gradient = math.tan(angle-math.pi/2)
+
+        planes.append(PlaneInfo(angle, pitch, noOfWires, translationFactor, sin, cos, gradient))
 
     return planes
 
+def wireNumberFromPoint(plane, point):
+    return math.floor((plane.cos * point.x + plane.sin * point.y - plane.cos * plane.translationFactor) / plane.pitch)
 
 def fireWires(planes, track):
     firedWires = []
@@ -84,15 +98,33 @@ def mergeEvent(event):
 
     return mergedEvent
 
+def makeLines(plane, wires):
+    dist0 = plane.pitch * wires[0]
+    dist1 = plane.pitch * wires[1]+1
 
+    if plane.translationFactor > 0:
+        point0 = Point(plane.translationFactor - (dist0*(-plane.cos)),dist0*plane.sin)
+        point1 = Point(plane.translationFactor - (dist1*(-plane.cos)),dist1*plane.sin)
+    else:
+        point0 = Point(dist0*plane.cos,dist0*plane.sin)
+        point1 = Point(dist1*plane.cos,dist1*plane.sin)
+
+    if plane.gradient == "INF":
+        line0 = [point0,Point(point0.x,point0.y+1)]
+        line1 = [point1,Point(point1.x,point1.y+1)]
+    else:
+        line0 = [point0,Point(point0.x+1,point0.y+plane.gradient)]
+        line1 = [point1,Point(point1.x+1,point1.y+plane.gradient)]
+
+    return [line0,line1]
+
+    return True
 
 # counterClockwise turn of axis
 def rotate(point, angle):
     return Point(point.x * math.cos(angle) + point.y * (math.sin(angle)), point.x * (-math.sin(angle)) + point.y * math.cos(angle))
 
 
-def wireNumberFromPoint(plane, point):
-    return math.floor((math.cos(plane.angle) * point.x + math.sin(plane.angle) * point.y - math.cos(plane.angle) * plane.translationFactor) / plane.pitch)
 
 # def checkCell(planes,wires):
 
@@ -132,9 +164,9 @@ volume = DetectorVolume(1000.0, 1000.0)
 wirePitches = [5.0, 5.0, 5.0]
 
 planes = generatePlaneInfo(wirePitches, volume)
-event = generateEvent(planes,volume)
-event = mergeEvent(event)
-pprint(event)
+# event = generateEvent(planes,volume)
+# event = mergeEvent(event)
+# pprint(event)
 # pprint(firedWires)
 # generateCells(planes,volume)
 # pprint(planes)
