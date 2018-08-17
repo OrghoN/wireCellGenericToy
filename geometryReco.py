f0040b4516a64b3614b02d686290bf505856f087
OrghoN <anoronyo@gmail.com>
2018-08-17 04:09:44 -0400

    cell reconstruction

--- geometryReco.py
+++ geometryReco.py
@@ -1,5 +1,11 @@
+#External Dependencies
+from scipy.spatial import ConvexHull
+import itertools
+
 #Internal Dependencies
 from dataTypes import *
+import geometryGen
+import utilities
 
 def makeLines(plane, wire):
     """Create lines based on a merged wire
@@ -37,3 +43,152 @@ def makeLines(plane, wire):
         line1 = Line(point1, Point(point1.x + 1, point1.y + plane.gradient))
 
     return [line0, line1]
+
+def lineIntersection(line0, line1):
+    """Gives points of intersection between 2 lines
+        Note: Doesn't account for parallel lines
+
+    Parameters
+    ----------
+    line0 : Line
+        First Line
+    line1 : Line
+        Second Line
+
+    Returns
+    -------
+    Point
+        Intersection Point
+
+    """
+    #Doesnt account for parallel line case
+    px = ((line0.point0.x * line0.point1.y - line0.point0.y * line0.point1.x) * (line1.point0.x - line1.point1.x) - (line0.point0.x - line0.point1.x) * (line1.point0.x * line1.point1.y -
+                                                                                                                                 line1.point0.y * line1.point1.x)) / ((line0.point0.x - line0.point1.x) * (line1.point0.y - line1.point1.y) - (line0.point0.y - line0.point1.y) * (line1.point0.x - line1.point1.x))
+    py = ((line0.point0.x * line0.point1.y - line0.point0.y * line0.point1.x) * (line1.point0.y - line1.point1.y) - (line0.point0.y - line0.point1.y) * (line1.point0.x * line1.point1.y -
+                                                                                                                                 line1.point0.y * line1.point1.x)) / ((line0.point0.x - line0.point1.x) * (line1.point0.y - line1.point1.y) - (line0.point0.y - line0.point1.y) * (line1.point0.x - line1.point1.x))
+    return Point(px, py)
+
+def wireIntersection(plane0, wire0, plane1, wire1):
+    """Intersection points between two merged wires in different planes
+
+    Parameters
+    ----------
+    plane0 : PlaneInfo
+        Plane information for the plane the wire 0 is for
+    wire : tuple[2] of int
+        A merged wire (wire0)
+    plane1 : PlaneInfo
+        Plane information for the plane the wire 1 is for
+    wire : tuple[2] of int
+        A merged wire (wire1)
+
+    Returns
+    -------
+    list of Point
+        Intersection Points Between merged wires
+
+    """
+    points = []
+    Lines0 = makeLines(plane0, wire0)
+    Lines1 = makeLines(plane1, wire1)
+
+    points.append(lineIntersection(Lines0[0],Lines1[0]))
+    points.append(lineIntersection(Lines0[1],Lines1[0]))
+    points.append(lineIntersection(Lines0[0],Lines1[1]))
+    points.append(lineIntersection(Lines0[1],Lines1[1]))
+
+    return points
+
+def sortPoints(points):
+    """Create convex hull and sort the points of convex hull in counterClockwise order
+
+    Parameters
+    ----------
+    points : list of Point
+        Input point cloud to make convex hull
+
+    Returns
+    -------
+    list of Point
+        List of points that form convex hull
+
+    """
+    sortedPoints = []
+    hull = ConvexHull(points)
+
+    for pointNo in (hull.vertices):
+        sortedPoints.append(points[pointNo])
+
+    return sortedPoints
+
+def checkCell(planes, wires):
+    """Check if the wires given form a cell
+
+    Parameters
+    ----------
+    planes : list of PlaneInfo
+        A list containing information for all the planes in the detector
+    wires : list of tuple[2] of int
+        A list of wires for each plane
+
+    Returns
+    -------
+    Cell
+        Cell(False, False) if it doesn't form a cell else the generated cell
+
+    """
+    points = []
+
+    #generate points for interections between wires from every plane
+    for plane0 in range(0,len(wires)):
+        for plane1 in  range(plane0+1,len(wires)):
+            potentialPoints = wireIntersection(planes[plane0], wires[plane0], planes[plane1], wires[plane1])
+
+            #check if point is in cell
+            for point in potentialPoints:
+                isPointInside = True
+                for planeNo, plane in enumerate(planes):
+                    if not utilities.pointInWire(plane,point,wires[planeNo]):
+                        isPointInside = False
+
+                if isPointInside:
+                    points.append(point)
+
+    #if less than or equal to 2 points, cell isn't real
+    if len(points) <=2:
+        return Cell(False, False)
+    else:
+        points = sortPoints(points)
+        potentialWires = list(itertools.chain(*geometryGen.mergeEvent(utilities.fireWires(planes,points))))
+        potentialWires = list(map(lambda x: (max((x[0][0],x[1][0])),min((x[0][1],x[1][1]))),zip(wires,potentialWires)))
+        return Cell(potentialWires,points)
+
+def reconstructCells(planes,event):
+    """Reconstruct cells from a merged event
+
+    Parameters
+    ----------
+    planes : list of PlaneInfo
+        A list containing information for all the planes in the detector
+    event : list of list of tuple[2] of int
+        List of merged wires in an event
+
+    Returns
+    -------
+    list of Cell
+        List of cells Reconstructed from Geometric information
+
+    """
+    cells = []
+    #cartesian product of all merged wires
+    potentialCells = list(itertools.product(*event))
+
+    #check if each potential combination is valid
+    for potentialCell in potentialCells:
+        cell = checkCell(planes, potentialCell)
+        if cell[0] == False:
+            continue
+        else:
+            cells.append(cell)
+
+    return cells
--- unitTests/test_geometryReco.py
+++ unitTests/test_geometryReco.py
@@ -14,3 +14,48 @@ def test_makeLines():
     ans = [Line(point0=Point(x=500.0, y=6.123233995736766e-14), point1=Point(x=500.0, y=1.0000000000000613)), Line(point0=Point(x=480.0, y=6.368163355566237e-14), point1=Point(x=480.0, y=1.0000000000000637))]
 
     assert geometryReco.makeLines(plane, wire) == ans
+
+def test_lineIntersection():
+    line0 = Line(point0=Point(x=500.0, y=6.123233995736766e-14), point1=Point(x=500.0, y=1.0000000000000613))
+    line1 = Line(point0=Point(x=250.00000000000006, y=433.0127018922193), point1=Point(x=251.00000000000006, y=432.43535162302965))
+
+    ans = Point(x=500.0, y=288.6751345948028)
+
+    assert geometryReco.lineIntersection(line0,line1)
+
+def test_wireIntersection():
+    plane0 = PlaneInfo(angle=1.0471975511965976, pitch=5.0, noOfWires=273, originTranslation=0, sin=0.8660254037844386, cos=0.5000000000000001, gradient=-0.5773502691896263)
+    plane1 = PlaneInfo(angle=3.141592653589793, pitch=5.0, noOfWires=200, originTranslation=1000.0, sin=1.2246467991473532e-16, cos=-1.0, gradient='INF')
+
+    wire0 = (100,103)
+    wire1 = (100,110)
+
+    ans = [Point(x=500.0, y=288.6751345948028), Point(x=500.0, y=311.7691453623879), Point(x=445.0, y=320.4293994002336), Point(x=445.0, y=343.5234101678187)]
+
+    assert geometryReco.wireIntersection(plane0, wire0, plane1, wire1) == ans
+
+def test_sortPoints():
+    points = [Point(x=261.7850716101253, y=245.02750845872833), Point(x=214.89166987825493, y=113.03402239905014), Point(x=0.2455514618699972, y=410.9473923892855), Point(x=581.1125709476491, y=493.9800164009199), Point(x=538.7063614584209, y=205.49539960120322)]
+
+    ans = [Point(x=581.1125709476491, y=493.9800164009199), Point(x=0.2455514618699972, y=410.9473923892855), Point(x=214.89166987825493, y=113.03402239905014), Point(x=538.7063614584209, y=205.49539960120322)]
+
+    assert geometryReco.sortPoints(points) == ans
+
+def test_checkCell():
+    planes = [PlaneInfo(angle=1.0471975511965976, pitch=5.0, noOfWires=273, originTranslation=0, sin=0.8660254037844386, cos=0.5000000000000001, gradient=-0.5773502691896263), PlaneInfo(angle=2.0943951023931953, pitch=5.0, noOfWires=273, originTranslation=1000.0, sin=0.8660254037844387, cos=-0.4999999999999998, gradient=0.5773502691896255), PlaneInfo(angle=3.141592653589793, pitch=5.0, noOfWires=200, originTranslation=1000.0, sin=1.2246467991473532e-16, cos=-1.0, gradient='INF')]
+    wires0 = [(41, 143), (81, 171), (83, 199)]
+    wires1 = [(0, 0), (100, 100), (100, 100)]
+
+    ans0 = Cell(wires=[(71, 143), (126, 171), (83, 199)], points=[Point(x=0.0, y=415.6921938164742), Point(x=585.0, y=493.6344801571189), Point(x=360.0000000000362, y=623.5382907247695)])
+    ans1 = Cell(False,False)
+
+    assert geometryReco.checkCell(planes, wires0) == ans0
+    assert geometryReco.checkCell(planes, wires1) == ans1
+
+def test_reconstructCells():
+    planes = [PlaneInfo(angle=1.0471975511965976, pitch=5.0, noOfWires=273, originTranslation=0, sin=0.8660254037844386, cos=0.5000000000000001, gradient=-0.5773502691896263), PlaneInfo(angle=2.0943951023931953, pitch=5.0, noOfWires=273, originTranslation=1000.0, sin=0.8660254037844387, cos=-0.4999999999999998, gradient=0.5773502691896255), PlaneInfo(angle=3.141592653589793, pitch=5.0, noOfWires=200, originTranslation=1000.0, sin=1.2246467991473532e-16, cos=-1.0, gradient='INF')]
+    event = [[(41, 143),(100, 100)],[(81, 171),(100, 100)],[(83, 199), (100, 100)]]
+
+    ans = [Cell(wires=[(71, 143), (126, 171), (83, 199)], points=[Point(x=0.0, y=415.6921938164742), Point(x=585.0, y=493.6344801571189), Point(x=360.0000000000362, y=623.5382907247695)]), Cell(wires=[(99, 100), (100, 100), (100, 100)], points=[Point(x=495.0, y=285.78838324887727), Point(x=500.0, y=288.6751345948255), Point(x=500.0, y=294.44863728670725), Point(x=495.0, y=291.561885940759)]), Cell(wires=[(100, 100), (100, 100), (100, 100)], points=[Point(x=495.0, y=285.78838324887727), Point(x=504.9999999999927, y=291.56188594076957), Point(x=500.0, y=294.4486372867136)])]
+
+    assert geometryReco.reconstructCells(planes,event) == ans
